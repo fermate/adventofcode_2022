a8dae775b2f1faac05fc22f764ae1235736d8ca7
fermate <fermate7@gmail.com>
2022-12-01 20:00:00 +0100

    The first day

---
+++ common.py
@@ -0,0 +1,30 @@
+def readFile(fileName):
+    fileContent = []
+    with open(fileName) as inputFile:
+        fileContent = inputFile.readlines()
+    
+    for i in range(len(fileContent)):
+        fileContent[i] = fileContent[i].rstrip()
+    
+    return fileContent
+
+def makeInt(array):
+    for i in range(len(array)):
+        if array[i]:
+            array[i] = int(array[i])
+    return array
+
+
+# a = 1
+# x = 1
+# g = True
+
+# while (g):
+#     if(13*a - 7*x > 1):
+#         x += 1
+#     elif(13*a - 7*x < 1):
+#         a += 1
+#     else:
+#         g = False
+
+# print(str(a) + " + " + str(x))
---
+++ day__1_1-2.py
@@ -0,0 +1,32 @@
+import re
+import common
+
+path = "inputs/day_1_input.txt"
+
+fileContent = common.readFile(path)
+
+fileContent = common.makeInt(fileContent)
+
+elfs = []
+
+calories = 0
+
+for line in fileContent:
+    if line:
+        calories = calories + line
+    else:
+        elfs.append(calories)
+        calories = 0
+
+elfs.append(calories)
+
+# only for part 1
+# max = 0
+# for elf in elfs:
+#     if elf > max:
+#         max = elf
+
+elfs.sort(reverse=True)
+
+
+print(elfs[0] + elfs[1] + elfs[2])
